db7900aa07d7dd5b7576cc13e1810cdd3d0732fa
Alberto <oropezabeto68@gmail.com>
2022-07-29 21:47:43 -0500

    ...

--- app.js
+++ app.js
@@ -226,4 +226,5 @@ app.post("/updatecita", (req, res) => {
 
 app.listen((process.env.PORT || 5000), (req, res) => {
   console.log("Server Running in http://localhost:3000");
+  console.log(precess.env.DB_USER);
 });
